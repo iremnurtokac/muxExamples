7c887dde7676b75f9f6dc0eb02d9b69a28d5f533
itokac <iremnur.tokac@zalando.de>
2017-03-16 17:16:09 +0100

    board game

--- exampleTic2/exampleTic2.go
+++ exampleTic2/exampleTic2.go
@@ -6,25 +6,25 @@ import (
 )
 
 func main() {
-	//Create a tic-tac-toe board
+	// Create a tic-tac-toe board.
 	board := [][]string{
 		[]string{"_", "_", "_"},
 		[]string{"_", "_", "_"},
 		[]string{"_", "_", "_"},
 	}
 
-	//The players take turns
-	board[0][0] = " "
-	board[0][1] = " "
-	board[0][1] = " "
-	board[1][0] = " "
-	board[1][1] = " "
-	board[1][2] = " "
-	board[2][0] = " "
-	board[2][1] = " "
-	board[2][2] = " "
+	// The players take turns.
+	board[0][0] = "_"
+	board[0][1] = "_"
+	board[0][2] = "_"
+	board[1][0] = "_"
+	board[1][1] = "X"
+	board[1][2] = "_"
+	board[2][0] = "_"
+	board[2][1] = "O"
+	board[2][2] = "X"
 
 	for i := 0; i < len(board); i++ {
-		fmt.Printf("%s/n", strings.Join(board[i], " "))
+		fmt.Printf("%s\n", strings.Join(board[i], " "))
 	}
 }
